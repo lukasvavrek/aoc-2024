f9373b525f38bb1ebbe1bbba919945911c004938
Lukas Vavrek <lvavrek7@gmail.com>
2024-12-05 00:08:20 +0100

    Day 4b - Haskell.

--- 4/main.hs
+++ 4/main.hs
@@ -8,16 +8,18 @@ import Data.Char
 main :: IO ()
 main = do
     input <- lines <$> getContents
-    let xs = findXs input
+    let xs = findChar input 'X'
     let xmasCount = sum $ map (containsXmas input) xs
     print xmasCount
+
+    let as = findChar input 'A'
+    let xmasCount' = length $ filter (isXmas' input) as
+    print xmasCount'
+
 
 data Coord = 
     X Int Int
     deriving (Show)
-
-findXs :: [String] -> [Coord]
-findXs input = findChar input 'X'
 
 findChar :: [String] -> Char -> [Coord]
 findChar input ch =
@@ -40,6 +42,20 @@ isXmas input (X x y) (xa, ya) =
     getAt input (x + xa, y + ya) == Just 'M' &&
     getAt input (x + 2 * xa, y + 2 * ya) == Just 'A' &&
     getAt input (x + 3 * xa, y + 3 * ya) == Just 'S'
+
+isXmas' :: [String] -> Coord -> Bool
+isXmas' input (X x y) =
+    ((getAt input (x - 1, y - 1) == Just 'S' &&
+      getAt input (x + 1, y + 1) == Just 'M') ||
+
+     (getAt input (x - 1, y - 1) == Just 'M' &&
+      getAt input (x + 1, y + 1) == Just 'S')) &&
+
+    ((getAt input (x - 1, y + 1) == Just 'S' &&
+      getAt input (x + 1, y - 1) == Just 'M') ||
+
+     (getAt input (x - 1, y + 1) == Just 'M' &&
+      getAt input (x + 1, y - 1) == Just 'S'))
 
 getAt :: [String] -> (Int, Int) -> Maybe Char
 getAt input (x, y)
